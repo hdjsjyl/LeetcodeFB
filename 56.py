c83653826aee2b9642f95357db2dba1f8c491eff
shilei <hdjsjyl@gmail.com>
2020-05-28 00:44:05 -0500

    TC: O(nlogn) for sorting the array MC: O(1), in-place operation

--- 56.py
+++ 56.py
@@ -9,7 +9,7 @@ Output: [[1,6],[8,10],[15,18]]
 Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
 """
 
-# solution: time complexity: O(nlogn) + O(n)
+# solution1: time complexity: O(nlogn) + O(n), memory complexity: O(n)
 # O(nlogn) is the time complexity of sorting algorithm
 # O(n) is the time complexity of accessing to the element
 class Solution:
@@ -35,3 +35,27 @@ class Solution:
                 start = start1
                 end = end1
         return res
+
+
+# solution2: time complexity: O(nlogn) + O(n), memory complexity: O(1)
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals = sorted(intervals, key=lambda x: x[0])
+        if len(intervals) <= 1:
+            return intervals
+
+        idx = 0
+        for i in range(1, len(intervals)):
+            start1 = intervals[idx][0]
+            end1 = intervals[idx][1]
+            s = intervals[i][0]
+            e = intervals[i][1]
+            if s <= end1:
+                start = min(start1, s)
+                end = max(end1, e)
+                intervals[idx] = [start, end]
+            else:
+                idx += 1
+                intervals[idx] = [s, e]
+
+        return intervals[:idx + 1]
