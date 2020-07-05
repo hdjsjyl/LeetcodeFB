1176c821ff369c48dec6a598dfd1276b9ed30ce4
shilei <hdjsjyl@gmail.com>
2020-07-05 02:43:26 -0500

    binary search, TC: O(logn)

--- 658.py
+++ 658.py
@@ -25,3 +25,25 @@ class Solution:
                 arr.pop(-1)
 
         return arr
+
+
+## binary search , TC: O(logn)
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        #         while len(arr) > k:
+        #             if x-arr[0] > arr[-1]-x:
+        #                 arr.pop(0)
+        #             else:
+        #                 arr.pop(-1)
+
+        #         return arr
+        left = 0
+        right = len(arr) - k
+        while left < right:
+            mid = left + (right - left) // 2
+            if x - arr[mid] > arr[mid + k] - x:
+                left = mid + 1
+            else:
+                right = mid
+
+        return arr[left:left + k]
